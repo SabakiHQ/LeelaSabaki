d733b53dd8c8b3e762bea179b78c9f22daabcc96
Yichuan Shen <shen.yichuan@gmail.com>
2017-11-27 23:37:22 +0100

    Implement basic Leela wrapper

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(process.argv)
+const {version} = require('./package')
+const commander = require('commander')
+const {spawn} = require('child_process')
+const readline = require('readline')
+
+commander
+.version(version)
+.arguments('<leelaPath> [leelaArgs...]')
+.action((leelaPath, leelaArgs) => {
+    const leela = spawn(leelaPath, ['--gtp', ...leelaArgs], {
+        stdio: 'inherit'
+    })
+})
+
+commander.parse(process.argv)
