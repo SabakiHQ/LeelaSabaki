6cdbd0507308accd641f9871d3acf47e165afa43
Yichuan Shen <yichuan.shen@sap.com>
2017-11-28 13:34:16 +0100

    Don't use commander

--- leelasabaki.js
+++ leelasabaki.js
@@ -1,15 +1,9 @@
-const {version} = require('./package')
-const commander = require('commander')
 const {spawn} = require('child_process')
-const readline = require('readline')
 
-commander
-.version(version)
-.arguments('<leelaPath> [leelaArgs...]')
-.action((leelaPath, leelaArgs) => {
-    const leela = spawn(leelaPath, ['--gtp', ...leelaArgs], {
-        stdio: 'inherit'
-    })
+if (process.argv.length < 3) return
+
+let [, , leelaPath, ...leelaArgs] = process.argv
+
+const leela = spawn(leelaPath, ['--gtp', ...leelaArgs], {
+    stdio: 'inherit'
 })
-
-commander.parse(process.argv)
